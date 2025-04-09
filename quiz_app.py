ad29d46963b6561685cd13ccd3d743caf7f18076
Garth Lo Bello <garthlobello1@gmail.com>
2025-04-09 13:01:10 -0400

    Update quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -259,4 +259,22 @@ def main():
         st.sidebar.write(f"JSON path: {updated_json_path}")
         if 'quiz' in st.session_state:
             st.sidebar.json({
-                "
+                "current_mode": st.session_state.quiz['mode'],
+                "selected_category": st.session_state.quiz['selected_category'],
+                "question_count": len(st.session_state.quiz.get('current_questions', [])),
+                "loaded_categories": list(st.session_state.quiz.get('all_questions', {}).keys())
+            })
+    
+    initialize_session_state()
+    
+    if st.session_state.quiz['mode'] == 'category_selection':
+        show_category_selection()
+    elif st.session_state.quiz['mode'] == 'question':
+        display_question()
+        if st.session_state.quiz['submitted']:
+            show_next_button()
+    else:
+        show_results()
+
+if __name__ == "__main__":
+    main()
